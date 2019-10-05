32ad514ec16e3e99392acdc549884b00bc803630
Juan Camilo Marin Navia <jcamilomnavia@hotmail.com>
2019-10-05 22:53:46 +0200

    Updates

---
+++ precache-manifest.1e7bc8f6ce3625c9e0b5a5a2f32eabc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2152f50a3eb7917813bdc7aa2759c0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dc9b59642dee1056ad0a",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "41ce61c3faf73bd98758",
+    "url": "/portfolio/static/css/main.33856302.chunk.css"
+  },
+  {
+    "revision": "dc9b59642dee1056ad0a",
+    "url": "/portfolio/static/js/2.6d48ee9c.chunk.js"
+  },
+  {
+    "revision": "41ce61c3faf73bd98758",
+    "url": "/portfolio/static/js/main.8c0e408e.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  },
+  {
+    "revision": "800d9a440f2caa0e0e7f56dc08317550",
+    "url": "/portfolio/static/media/profile.800d9a44.jpg"
+  },
+  {
+    "revision": "ce4de1d9c24902a990951d2995f95c5f",
+    "url": "/portfolio/static/media/walkingbehind.ce4de1d9.jpg"
+  }
+]);
